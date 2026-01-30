edb6f9a7cfa038dccceaa15f6ea2419b36b6717b
Javier Míguez Lalín <javiermiguezlalin@gmail.com>
2026-01-30 20:04:57 +0100

    test(users): add unit tests for UserProfile and factory

---
+++ Users.Tests/Unit/Domain/TestFactories/UserProfileTestFactory.cs
@@ -0,0 +1,21 @@
+﻿using Users.Domain;
+
+namespace Users.Tests.Unit.Domain.TestFactories;
+
+public static class UserProfileTestFactory
+{
+    public static UserProfile CreateTestProfile(
+        Guid? userId = null,
+        UserType type = UserType.Player,
+        string firstName = "Miro",
+        string lastName = "Pereira",
+        PlayerRank? rank = PlayerRank.Beginner)
+    {
+        return new UserProfile(
+            userId ?? Guid.NewGuid(),
+            type,
+            new FullName(firstName, lastName),
+            rank
+        );
+    }
+}
